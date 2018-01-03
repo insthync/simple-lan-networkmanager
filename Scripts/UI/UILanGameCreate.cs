e1070ea617b0032f1c981272fb778a101cbfc77e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-04 01:39:13 +0700

    Add Lan game creation dialog

---
+++ Scripts/UI/UILanGameCreate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILanGameCreate : UIBase
+{
+    public MapData[] maps;
+    public static readonly Dictionary<string, MapData> Maps = new Dictionary<string, MapData>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Maps.Clear();
+        foreach (var map in maps)
+        {
+            Maps[map.GetId()] = map;
+        }
+    }
+}
