6754c2d41e712192032bfe97533b534001e88321
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-29 20:16:50 +0700

    Add ui game scores

---
+++ Scripts/UI/UINetworkGameScores.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINetworkGameScores : MonoBehaviour
+{
+    public UINetworkGameScoreEntry[] userRankings;
+    public UINetworkGameScoreEntry localRanking;
+
+    public void UpdateRankings(NetworkGameScore[] rankings)
+    {
+        for (var i = 0; i < userRankings.Length; ++i)
+        {
+            var userRanking = userRankings[i];
+            if (i < rankings.Length)
+            {
+                var ranking = rankings[i];
+                userRanking.SetData(i + 1, ranking);
+
+                var isLocal = BaseNetworkGameCharacter.Local != null && ranking.netId.Equals(BaseNetworkGameCharacter.Local.netId);
+                if (isLocal)
+                    UpdateLocalRank(i + 1, ranking);
+            }
+            else
+                userRanking.Clear();
+        }
+    }
+
+    public void UpdateLocalRank(int rank, NetworkGameScore ranking)
+    {
+        if (localRanking != null)
+            localRanking.SetData(rank, ranking);
+    }
+}
